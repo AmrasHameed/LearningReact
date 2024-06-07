677812e0a54fbe2201018bf5c3f0c6ca6b0f2647
AmrasHameed <amrazhameed@gmail.com>
2024-06-07 12:34:17 +0530

    Add .gitattributes to normalize line endings

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement('div',{id:'parent'},
     [React.createElement('div',{id:'child1'},
     [React.createElement('h1',{},'This is an h1 tag'),React.createElement('h2',{},'This is an h2 tag')]),
